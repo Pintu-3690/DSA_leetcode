8f866c9f37346351491bc161b701a34fe50d8b84
Pintu <deadpoolpintu3690@gmail.com>
2026-08-05 20:36:41 +0530

    Time: 4 ms (30.85%) | Memory: 8.5 MB (65.49%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if ( x < 0 ) return false;
+    int y = x;
+    long long reverse = 0;
+    int lastDigit = 0;
+    while( x > 0){
+        lastDigit = x%10;
+        reverse = (reverse * 10)+lastDigit;
+        x = x/10; 
+    }
+    return y == reverse;
+    }
+};
